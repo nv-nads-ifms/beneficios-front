59a5381f993b1f9fce803d3be535774273b4271d
Nicholas Eduardo Lopes dos Santos <nicholaseduardo@yahoo.com.br>
2023-04-24 10:14:23 -0400

    ajuste da chamada do método de busca adicionando o /list

--- src/services/MenuService.js
+++ src/services/MenuService.js
@@ -5,7 +5,7 @@ const MENU_API_BASE_URL = "/menus";
 class MenuService {
     
     getMenus() {
-        return getData(MENU_API_BASE_URL);
+        return getData(MENU_API_BASE_URL+"/list");
     }
     
 }
