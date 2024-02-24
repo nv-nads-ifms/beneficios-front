12190f991e14a79776071144bfc3b3f6bfc0bc74
Prof. Nicholas Eduardo Lopes dos Santos <nicholas.santos@ifms.edu.br>
2024-02-24 13:22:48 -0400

    ajustes do componente para permitir a inclusão de dados caso a listagem esteja vazia

--- src/components/V1.0.0/DNAAutocomplete.jsx
+++ src/components/V1.0.0/DNAAutocomplete.jsx
@@ -1,17 +1,40 @@
 import React from "react";
-import { Autocomplete, CircularProgress, TextField } from "@mui/material";
+import PropTypes from 'prop-types';
+import { formContext } from "../../contexts/formContext";
+import { Autocomplete, CircularProgress, InputAdornment, TextField } from "@mui/material";
 import DataService from "../../api/services/DataServices";
+import AddIconButton from "../CustomIconButtons/AddIconButton";
+import DNABaseDialogForm from "./forms/DNABaseDialogForm";
+import { DNAStatus } from "../../api/utils/constants";
 
-export default function DNAAutocomplete(props) {
-    const { path, input_label } = props;
+function DNAAutocomplete(props) {
+    const { path, input_label, input_modal, input_modal_title, input_handle_modal } = props;
+
+    /* Atributos de controle do formulário */
+    const [openModal, setOpenModal] = React.useState(false);
+    const [formId, setFormId] = React.useState(0);
+
+    /* controle de edição do formulário */
+    const dataControl = React.useMemo(() => {
+        return props.disabled ? DNAStatus.VIEW : DNAStatus.EDIT;
+    }, [props.disabled]);
 
     const [open, setOpen] = React.useState(false);
     const [options, setOptions] = React.useState([]);
     const loading = open && options.length === 0;
-    
+
+
     const dataService = React.useMemo(() => {
         return new DataService(`/${path}`);
     }, [path]);
+
+    const handleClose = () => {
+        setOpenModal(false);
+    };
+
+    const onShowCadastro = () => {
+        setOpenModal(true);
+    }
 
     React.useEffect(() => {
         let active = true;
@@ -35,7 +58,7 @@ export default function DNAAutocomplete(props) {
         return () => {
             active = false;
         };
-    }, [loading, dataService]);
+    }, [formId, loading, dataService]);
 
     React.useEffect(() => {
         if (!open) {
@@ -44,29 +67,62 @@ export default function DNAAutocomplete(props) {
     }, [open]);
 
     return (
-        <Autocomplete
-            open={open}
-            onOpen={() => setOpen(true)}
-            onClose={() => setOpen(false)}
+        <formContext.Provider value={{
+            setFormId: setFormId,
+            // setDataControl: setDataControl,
+            setOpen: setOpenModal
+        }}>
+            <Autocomplete
+                open={open}
+                onOpen={() => setOpen(true)}
+                onClose={() => setOpen(false)}
 
-            options={options}
-            loading={loading}
-            renderInput={(params) => (
-                <TextField
-                    {...params}
-                    label={input_label}
-                    InputProps={{
-                        ...params.InputProps,
-                        endAdornment: (
-                            <React.Fragment>
-                                {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                                {params.InputProps.endAdornment}
-                            </React.Fragment>
-                        ),
-                    }}
-                />
-            )}
-            {...props}
-        />
+                options={options}
+                loading={loading}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label={input_label}
+                        InputProps={{
+                            ...params.InputProps,
+                            startAdornment: (props.startAdornment),
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                    {params.InputProps.endAdornment}
+                                    {input_modal != null && (
+                                        <AddIconButton
+                                            disabled={props.disabled}
+                                            tooltip="Adicionar uma nova opção"
+                                            onClick={input_handle_modal != null ? input_handle_modal : onShowCadastro} />
+                                    )}
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                )}
+                {...props}
+            />
+
+            <DNABaseDialogForm
+                id_value={formId}
+                datacontrol={dataControl}
+                text={input_modal_title}
+                
+                open={openModal}
+                on_close_func={handleClose}
+                data_source_url={path}
+            />
+        </formContext.Provider>
     );
 }
+
+DNAAutocomplete.propTypes = {
+    path: PropTypes.string.isRequired,
+    input_label: PropTypes.string.isRequired,
+    input_modal: PropTypes.bool, 
+    input_modal_title: PropTypes.string,
+    input_handle_modal: PropTypes.func
+};
+
+export default DNAAutocomplete;
